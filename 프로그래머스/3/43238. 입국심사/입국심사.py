21ae7ea63f016af00036151e492f906331ffc2cb
Junho Jang <104313883+imjjh@users.noreply.github.com>
2026-05-15 18:26:41 +0900

    [level 3] Title: 입국심사, Time: 257.99 ms, Memory: 13.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,22 @@
+def solution(n, times):
+    start = 0
+    end = 1_000_000_000 ** 2
+    while start < end:
+        mid = (start + end) // 2
+        
+        people = 0
+        for time in times:
+            people += mid // time
+    
+        if n <= people:
+            end = mid
+            answer = mid
+        
+        elif n > people:
+            start = mid + 1
+            
+    return answer
+            
+            
+        
+    
